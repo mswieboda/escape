b158a8930270a0af593d540bdbbe7fee6f05a17b
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-23 22:01:10 -0500

    adds initial floor tiles

--- source/escape/PlayState.hx
+++ source/escape/PlayState.hx
@@ -2,21 +2,37 @@ package escape;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.tile.FlxTilemap;
 
 class PlayState extends FlxState {
   var player: Player;
+  var tiles: FlxTilemap;
+
+  static inline var TILE_WIDTH = 32;
+  static inline var TILE_HEIGHT = 32;
 
   override public function create() {
-    player = new Player(30, 30);
-
-    add(player);
-
     FlxG.mouse.visible = false;
+
+    tiles = new FlxTilemap();
+    tiles.loadMapFromCSV(
+      "assets/tiles/level.txt",
+      "assets/tiles/tiles.png",
+      TILE_WIDTH,
+      TILE_HEIGHT,
+      AUTO
+    );
+    add(tiles);
+
+    player = new Player(30, 30);
+    add(player);
 
     super.create();
   }
 
   override function update(elapsed: Float) {
+    FlxG.collide(player, tiles);
+
     super.update(elapsed);
   }
 }
