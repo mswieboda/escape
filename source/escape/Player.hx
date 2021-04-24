3c7449e185312769ccf8f516bac1a27dff3f2a14
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-23 22:37:03 -0500

    adds player jump

--- source/escape/Player.hx
+++ source/escape/Player.hx
@@ -4,11 +4,15 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.input.keyboard.FlxKey;
 import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
 
 class Player extends FlxSprite {
-  static inline var MOVEMENT_SPEED = 100;
-  static inline var DRAG: Int = 1000;
-  static inline var WALK_FPS = 6;
+  static inline var MOVEMENT_ACCELERATION = 640;
+  static inline var JUMP_SPEED = 320;
+  static inline var DRAG: Int = 640;
+  static inline var GRAVITY = 640;
+  static inline var MAX_VELOCITY_X = 160;
+  static inline var MAX_VELOCITY_Y = 640;
 
   var direction: Int;
 
@@ -19,11 +23,9 @@ class Player extends FlxSprite {
 
     makeGraphic(32, 32, FlxColor.LIME);
 
-    // loadGraphic(AssetPaths.player__png, true, 32, 32);
-
-    // animation.add("walk", [0, 2, 1, 2], WALK_FPS, false);
-
-    drag.x = drag.y = DRAG;
+    drag.x = DRAG;
+    acceleration.y = GRAVITY;
+    maxVelocity.set(MAX_VELOCITY_X, MAX_VELOCITY_Y);
   }
 
   override public function update(elapsed: Float) {
@@ -37,33 +39,29 @@ class Player extends FlxSprite {
     var rights = [FlxKey.RIGHT, FlxKey.D];
     var left = FlxG.keys.anyPressed(lefts);
     var right = FlxG.keys.anyPressed(rights);
+    var jump = FlxG.keys.anyJustPressed([UP, W]);
 
     if (left && right) left = right = false;
 
-    if (FlxG.keys.anyJustReleased(lefts.concat(rights))) {
-      // animation.pause();
-    }
+    acceleration.x = 0;
 
-    if (!(left || right)) return;
-
-    var vx = velocity.x;
-    var vy = velocity.y;
+    FlxSpriteUtil.bound(this);
 
     if (left) {
-      vx = -MOVEMENT_SPEED;
+      acceleration.x -= MOVEMENT_ACCELERATION;
       facing = LEFT;
     } else if (right) {
-      vx = MOVEMENT_SPEED;
+      acceleration.x += MOVEMENT_ACCELERATION;
       facing = RIGHT;
     }
 
-    // animation.play(facingToAnimation());
-
-    var distance = Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
-
-    vx = vx / distance * MOVEMENT_SPEED;
-
-    velocity.set(vx, vy);
+    // TODO: check `velocity.y == 0` etc (not currently working, it's `7`)
+    // to see if they're on the floor, before jumping
+    if (jump) {
+      // so we're not initially colliding with the floor
+      y -= 1;
+      velocity.y = -JUMP_SPEED;
+    }
   }
 
   function facingToAnimation() {
