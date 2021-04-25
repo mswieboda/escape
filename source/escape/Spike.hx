4c6cbae485063bd9f1838cfe96dc45257433a2bf
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-24 23:18:08 -0500

    adds spike sprite

--- source/escape/Spike.hx
+++ source/escape/Spike.hx
@@ -10,7 +10,7 @@ class Spike extends FlxSprite {
   public function new(x: Float, y: Float) {
     super(x, y);
 
-    makeGraphic(WIDTH, HEIGHT, FlxColor.GRAY);
+    loadGraphic(AssetPaths.spike__png, false, WIDTH, HEIGHT);
 
     // keeps it static at the top, kind of like a HUD
     // instead of `setScroll(0, 0);` since that gets ignored by collisions
