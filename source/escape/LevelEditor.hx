a402a9522868deee9f238221c57d6a1055b60009
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-04-28 21:19:42 -0500

    adds level editor cursor

--- source/escape/LevelEditor.hx
+++ source/escape/LevelEditor.hx
@@ -1,28 +1,74 @@
 package escape;
 
 import flixel.system.FlxAssets;
+import flixel.FlxSprite;
+import flixel.util.FlxSpriteUtil;
+import flixel.util.FlxColor;
+
 
 class LevelEditor extends BaseLevel {
+  static inline var TILE_WIDTH = BaseLevel.TILE_WIDTH;
+  static inline var TILE_HEIGHT = BaseLevel.TILE_HEIGHT;
+  static inline var CURSOR_THICKNESS = 5.0;
+  static inline var CURSOR_COLOR = 0xFF00FF00;
+
+  var cursor: FlxSprite;
+
   public function new(
     player: Player,
     levelData: String,
     tileGraphic: FlxTilemapGraphicAsset
   ) {
+    makeCursor();
+
     super(player, levelData, tileGraphic);
+  }
 
+  function makeCursor() {
+    cursor = new FlxSprite();
 
+    var width = Std.int(TILE_WIDTH + CURSOR_THICKNESS);
+    var height = Std.int(TILE_HEIGHT + CURSOR_THICKNESS);
+
+    cursor.makeGraphic(width, height, FlxColor.TRANSPARENT);
+
+    FlxSpriteUtil.drawRect(
+      cursor,
+      CURSOR_THICKNESS / 2,
+      CURSOR_THICKNESS / 2,
+      TILE_WIDTH,
+      TILE_HEIGHT,
+      FlxColor.TRANSPARENT,
+      {thickness: CURSOR_THICKNESS, color: CURSOR_COLOR}
+    );
+  }
+
+  override function addAll() {
+    trace('LevelEditor addAll');
+    super.addAll();
+
+    add(cursor);
   }
 
   override function update(elapsed: Float) {
     super.update(elapsed);
 
-    // player.updateBeforeCollisionChecks(elapsed);
+    updateCursorPosition();
+  }
 
-    // FlxG.collide(colliders, player);
-    // FlxG.collide(spikes, player, Player.onHitSpikes);
-    // FlxG.overlap(doorTriggers, player, Player.onDoorTrigger, Door.onDoorTrigger);
-    // FlxG.overlap(ladderTriggers, player, Player.onLadderTrigger);
-    // FlxG.overlap(leftWallJumpTriggers, player.feetTrigger, player.onLeftWallJumpTrigger);
-    // FlxG.overlap(rightWallJumpTriggers, player.feetTrigger, player.onRightWallJumpTrigger);
+  function updateCursorPosition() {
+    var up = Actions.menu.up.triggered;
+    var down = Actions.menu.down.triggered;
+    var left = Actions.menu.left.triggered;
+    var right = Actions.menu.right.triggered;
+
+    if (up && down) up = down = false;
+    if (left && right) left = right = false;
+
+    if (up || down) {
+      cursor.y += up ? -TILE_HEIGHT : TILE_HEIGHT;
+    } else if (left || right) {
+      cursor.x += left ? -TILE_WIDTH : TILE_WIDTH;
+    }
   }
 }
--- source/escape/MenuActions.hx
+++ source/escape/MenuActions.hx
@@ -7,18 +7,24 @@ import flixel.input.actions.FlxAction.FlxActionDigital;
 class MenuActions {
   public static var up: FlxActionDigital;
   public static var down: FlxActionDigital;
+  public static var left: FlxActionDigital;
+  public static var right: FlxActionDigital;
   public static var action: FlxActionDigital;
   public static var cancel: FlxActionDigital;
 
   public static function addInputs(actions: ActionManager) {
     up = new FlxActionDigital();
     down = new FlxActionDigital();
+    left = new FlxActionDigital();
+    right = new FlxActionDigital();
     action = new FlxActionDigital();
     cancel = new FlxActionDigital();
 
     actions.addActions([
       up,
       down,
+      left,
+      right,
       action,
       cancel
     ]);
@@ -29,6 +35,10 @@ class MenuActions {
     up.addKey(TAB, JUST_PRESSED);
     down.addKey(DOWN, JUST_PRESSED);
     down.addKey(S, JUST_PRESSED);
+    left.addKey(LEFT, JUST_PRESSED);
+    left.addKey(A, JUST_PRESSED);
+    right.addKey(RIGHT, JUST_PRESSED);
+    right.addKey(D, JUST_PRESSED);
     // TODO: add combo of SHIFT+TAB for down
     // down.addKey(SHIFT + TAB, JUST_PRESSED);
     action.addKey(ENTER, JUST_PRESSED);
@@ -40,6 +50,8 @@ class MenuActions {
     // Add gamepad DPAD inputs
     up.addGamepad(DPAD_UP, JUST_PRESSED);
     down.addGamepad(DPAD_DOWN, JUST_PRESSED);
+    left.addGamepad(DPAD_LEFT, JUST_PRESSED);
+    right.addGamepad(DPAD_RIGHT, JUST_PRESSED);
     action.addGamepad(A, JUST_PRESSED);
     action.addGamepad(START, JUST_PRESSED);
     cancel.addGamepad(B, JUST_PRESSED);
@@ -48,5 +60,7 @@ class MenuActions {
     // Add gamepad analog stick (as simulated DPAD) inputs
     up.addGamepad(LEFT_STICK_DIGITAL_UP, JUST_PRESSED);
     down.addGamepad(LEFT_STICK_DIGITAL_DOWN, JUST_PRESSED);
+    left.addGamepad(LEFT_STICK_DIGITAL_LEFT, JUST_PRESSED);
+    right.addGamepad(LEFT_STICK_DIGITAL_RIGHT, JUST_PRESSED);
   }
 }
